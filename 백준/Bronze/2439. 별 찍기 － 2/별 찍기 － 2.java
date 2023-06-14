51fdb605d2b187abec7bfc77eb71be4e0472261c
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-06-14 22:22:33 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 240 ms, Memory: 12936 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		//StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 1; j < N-i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+
+		
+
+	}
+
+}
