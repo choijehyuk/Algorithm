89d9f6fc5cf9d728d52f03359e19dbc1c616fabf
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-06-14 22:33:01 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 96 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int[] A;
+	static int[] dp;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		A = new int[N];
+		dp = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int answer = dp();
+		// System.out.println(Arrays.toString(A));
+		//System.out.println(Arrays.toString(dp));
+		System.out.println(answer);
+
+	}
+
+	private static int dp() {
+		dp[0] = 1;
+		for (int i = 1; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (A[i] < A[j] && dp[i] <= dp[j]) {
+					dp[i] = dp[j] + 1;
+				} 
+			}
+		}
+
+		Arrays.sort(dp);
+		return dp[N - 1];
+	}
+
+}
