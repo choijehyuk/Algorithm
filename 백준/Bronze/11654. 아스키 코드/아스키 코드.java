5625e7a02ba3ad85bd2d87c55c14f63a61e28754
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-06-14 22:32:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		
+		String s = br.readLine();
+		char ch = s.charAt(0);
+		System.out.println((int)ch);
+	}
+}
