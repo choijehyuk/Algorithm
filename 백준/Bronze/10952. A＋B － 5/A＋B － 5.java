6a23e240744590388c7d4bd35dcf2db39b9721fd
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-06-14 22:31:11 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 13188 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		
+		Scanner in=new Scanner(System.in);
+		
+		while(true){
+		
+			int A=in.nextInt();
+			int B=in.nextInt();
+		
+			if(A==0 && B==0){
+				in.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+	}
+}
