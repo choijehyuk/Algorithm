49cc4fe11195ff1710cf6d6c0ca61a03bec10ede
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-07-17 14:57:27 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 68 ms, Memory: 11280 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,20 @@
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
