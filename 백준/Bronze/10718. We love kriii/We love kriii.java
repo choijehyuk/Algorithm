4ba48d11c060bc59539db96c65e180301c3a8b9b
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-07-28 08:44:28 +0900

    [Bronze V] Title: We love kriii, Time: 76 ms, Memory: 11420 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,17 @@
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        System.out.println("강한친구 대한육군\n"
+                + "강한친구 대한육군");
+    }
+}
