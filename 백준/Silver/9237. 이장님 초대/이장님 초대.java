44a4f557d2e6397f4330c5bb3fe5c9ee69da8546
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-09-21 01:45:30 +0900

    [Silver V] Title: 이장님 초대, Time: 356 ms, Memory: 29228 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int H, W;
+    static char[][] map;
+    static boolean[][] v;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int day = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            arr[N - i - 1] = arr[N - i - 1] + i + 1;
+            day = Math.max(day, arr[N - i - 1]);
+        }
+
+        System.out.println(day + 1);
+
+
+    }
+
+}
