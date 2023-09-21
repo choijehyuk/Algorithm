623d27360e44a3dd06365d3193144a436f29a187
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-09-22 02:07:57 +0900

    [Silver V] Title: BABBA, Time: 84 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+2];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n-1] + " " + dp[n]);
+
+
+    }
+
+}
