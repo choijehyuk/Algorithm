43667f9891a5aad625b92223ca14749985b892d9
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-10-28 18:40:36 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 21388 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int K = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        for (Integer i : stack) {
+            answer += i;
+        }
+
+        System.out.println(answer);
+
+    }
+
+
+}
