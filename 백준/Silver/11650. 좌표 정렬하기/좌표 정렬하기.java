76d34871e23be24c7bb25a7236e39c8f1cc597d1
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-11-06 21:41:47 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1904 ms, Memory: 62404 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        List<Point> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+
+            list.add(new Point(A, B));
+        }
+
+        Collections.sort(list, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                if (o1.x == o2.x) {
+                    return Integer.compare(o1.y, o2.y);
+                }
+                return Integer.compare(o1.x, o2.x);
+            }
+        });
+
+        for (Point point : list) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
