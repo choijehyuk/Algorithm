ec3f3650ba0e4b59e9a854c5a0a8eb3af1308a00
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-11-11 14:34:13 +0900

    [level 1] Title: 추억 점수, Time: 0.05 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++){
+            map.put(name[i],yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++){
+            int sum = 0;
+            for(int j = 0; j < photo[i].length; j++){
+               if(map.get(photo[i][j]) == null){
+                   sum += 0;
+               }
+                else{
+                sum += map.get(photo[i][j]);
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
