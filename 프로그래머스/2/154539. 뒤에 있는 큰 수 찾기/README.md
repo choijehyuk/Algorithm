adc43644195eed75a1e2edb8b969434a65a2b889
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-11-23 13:33:31 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 95.61 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 뒤에 있는 큰 수 찾기 - 154539 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 13:33:11
+2023년 11월 4일 13:33:29
 
 ### 문제 설명
 
--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,45 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
-
-    static class Point implements Comparable<Point> {
-        int num, idx;
-        public Point(int num, int idx) {
-            this.num = num;
-            this.idx = idx;
-        }
-
-        @Override
-        public int compareTo(Point other) {
-            return Integer.compare(this.num, other.num);
-        }
-    }
-    
     public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        
-        PriorityQueue<Point> q = new PriorityQueue<>();
-        
-        Arrays.fill(answer, -1);
-        for(int i = 0; i < numbers.length; i++){
-            int check = numbers[i];
-            q.add(new Point(check,i));
-            
-            int size = q.size();
-            for(int j = 0; j < size; j++){
-                Point p = q.poll();
-
-                if(p.num < check){
-                    answer[p.idx] = check;
-                }
-                else{
-                    q.add(new Point(p.num, p.idx));
-                    break;
-                }
-            }
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
