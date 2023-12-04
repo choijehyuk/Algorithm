8b11cdd4ec70e8c16155d7022cad328d0bb512a8
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-12-04 12:11:50 +0900

    [Bronze I] Title: 약수, Time: 80 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        if (N == 1) {
+            System.out.println(arr[0] * arr[0]);
+        } else {
+            System.out.println(arr[0] * arr[N - 1]);
+        }
+    }
+}
