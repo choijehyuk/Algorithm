439d09761a84b0a4c61620c2ecaec6c557452d5e
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2023-12-13 15:56:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,83 +1,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int N, K;
+    static int v[];
 
-	static int N, K;
-	static int v[];
+    static class Point {
+        int n;
+        int cnt;
 
-	static class Point {
-		int n;
-		int cnt;
+        public Point(int n, int cnt) {
+            super();
+            this.n = n;
+            this.cnt = cnt;
+        }
 
-		public Point(int n, int cnt) {
-			super();
-			this.n = n;
-			this.cnt = cnt;
-		}
+    }
 
-	}
+//    static int fast = Integer.MAX_VALUE;
+//    static int cnt = 0;
 
-	static int fast = Integer.MAX_VALUE;
-	static int cnt = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-		bfs();
-		System.out.println(fast);
-		//System.out.println(cnt);
-	}
+        bfs();
+        System.out.println(v[K] - 1);
+    }
 
-	private static void bfs() {
-		Queue<Point> q = new LinkedList<>();
-		q.offer(new Point(N, 0));
-		v = new int[100001];
-		v[N] = 1;
-		while (!q.isEmpty()) {
-			Point p = q.poll();
-			int pn = p.n;
-			if (pn == K) {
-				// System.out.println(fast + " " + p.cnt);
-				fast = Math.min(fast, p.cnt);
-			}
+    private static void bfs() {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(N, 1));
+        v = new int[100001];
+        v[N] = 1;
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            int pn = p.n;
 
-			pn = 0;
-			if(p.n < 100) {
-				//System.out.println(p.n + " " + p.cnt);
-			}
-			pn = p.n * 2;
-			if (pn >= 0 && pn < 100001) {
-				if (v[pn] == p.cnt + 1 || v[pn] == 0) {
-					q.offer(new Point(pn, p.cnt));
-					v[pn] = p.cnt;
-				}
-			}
-			
-			pn = p.n + 1;
-			if (pn >= 0 && pn < 100001) {
-				if (v[pn] == p.cnt + 1 || v[pn] == 0) {
-					q.offer(new Point(pn, p.cnt + 1));
-					v[pn] = p.cnt;
-				}
-			}
-			
-			pn = p.n - 1;
-			if (pn >= 0 && pn < 100001) {
-				if (v[pn] == p.cnt + 1 || v[pn] == 0) {
-					q.offer(new Point(pn, p.cnt + 1));
-					v[pn] = p.cnt;
-				}
-			}
-		}
-	}
+//            if (pn == K) {
+//                // System.out.println(fast + " " + p.cnt);
+//                fast = Math.min(fast, p.cnt);
+//                return;
+//            }
 
+            pn = p.n * 2;
+            if (pn >= 0 && pn < 100001) {
+                if (v[pn] > p.cnt || v[pn] == 0) {
+                    q.offer(new Point(pn, p.cnt));
+                    v[pn] = p.cnt;
+                }
+            }
+
+            pn = p.n + 1;
+            if (pn >= 0 && pn < 100001) {
+                if (v[pn] > p.cnt + 1 || v[pn] == 0) {
+                    q.offer(new Point(pn, p.cnt + 1));
+                    v[pn] = p.cnt + 1;
+                }
+            }
+
+            pn = p.n - 1;
+            if (pn >= 0 && pn < 100001) {
+                if (v[pn] > p.cnt + 1 || v[pn] == 0) {
+                    q.offer(new Point(pn, p.cnt + 1));
+                    v[pn] = p.cnt + 1;
+                }
+            }
+        }
+    }
 }
