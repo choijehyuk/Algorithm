f25d1cbeb25ccde6b91bd5877a4edad21a546a95
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-01-02 12:27:08 +0900

    [Gold IV] Title: 전화번호 목록, Time: 556 ms, Memory: 241412 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -14,147 +14,72 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for (int tc = 0; tc < T; tc++) {
+
             int N = Integer.parseInt(br.readLine());
-            ArrayList<String> list = new ArrayList<>();
-            ArrayList<String> zerolist = new ArrayList<>();
-            ArrayList<String> onelist = new ArrayList<>();
-            ArrayList<String> twolist = new ArrayList<>();
-            ArrayList<String> threelist = new ArrayList<>();
-            ArrayList<String> fourlist = new ArrayList<>();
-            ArrayList<String> fivelist = new ArrayList<>();
-            ArrayList<String> sixlist = new ArrayList<>();
-            ArrayList<String> sevenlist = new ArrayList<>();
-            ArrayList<String> eightlist = new ArrayList<>();
-            ArrayList<String> ninelist = new ArrayList<>();
+            TrieNode trie = new TrieNode();
+            boolean isConsistent = true;
 
+            List<String> keyList = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                String str = br.readLine();
+                trie.insert(str);
+                keyList.add(str);
+            }
 
-            for (int i = 0; i < N; i++) {
-                String input = br.readLine();
-
-                if (input.charAt(0) == '0') {
-                    zerolist.add(input);
-                } else if (input.charAt(0) == '1') {
-                    onelist.add(input);
-                } else if (input.charAt(0) == '2') {
-                    twolist.add(input);
-                } else if (input.charAt(0) == '3') {
-                    threelist.add(input);
-                } else if (input.charAt(0) == '4') {
-                    fourlist.add(input);
-                } else if (input.charAt(0) == '5') {
-                    fivelist.add(input);
-                } else if (input.charAt(0) == '6') {
-                    sixlist.add(input);
-                } else if (input.charAt(0) == '7') {
-                    sevenlist.add(input);
-                } else if (input.charAt(0) == '8') {
-                    eightlist.add(input);
-                } else if (input.charAt(0) == '9') {
-                    ninelist.add(input);
+            for (String key : keyList) {
+                if (trie.contains(key)) {
+                    isConsistent = false;
+                    break;
                 }
-
             }
-
-            sort(zerolist);
-            sort(onelist);
-            sort(twolist);
-            sort(threelist);
-            sort(fourlist);
-            sort(fivelist);
-            sort(sixlist);
-            sort(sevenlist);
-            sort(eightlist);
-            sort(ninelist);
-
-
-            boolean bool = true;
-
-            bool = sub(zerolist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-
-            bool = sub(onelist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(twolist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(threelist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(fourlist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(fivelist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(sixlist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(sevenlist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(eightlist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-            bool = sub(ninelist);
-            if (!bool){
-                sb.append("NO\n");
-                continue;
-            }
-
-            if(bool) sb.append("YES\n");
+            System.out.println(isConsistent ? "YES" : "NO");
         }
-
-        System.out.println(sb);
     }
 
-    private static boolean sub(ArrayList<String> list) {
-        int idx = 0;
 
-        while (idx < list.size()) {
-            String check = list.get(idx);
-            int size = check.length();
+    private static class TrieNode {
+        Map<Character, TrieNode> childNode = new HashMap<>();
+        boolean terminal;
 
-            for (int i = idx + 1; i < list.size(); i++) {
-                String num = list.get(i);
-                if (num.substring(0, size).equals(check)) {
+        TrieNode() {
+        }
+
+        public void insert(String word) {
+            TrieNode trieNode = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+
+                // tmp childNode에 c없으면 추가
+                trieNode.childNode.putIfAbsent(c, new TrieNode());
+                trieNode = trieNode.childNode.get(c);
+
+                // 마지막 문자 terminal
+                if (i == word.length() - 1) {
+                    trieNode.terminal = true;
+                    return;
+                }
+            }
+        }
+
+        public boolean contains(String word) {
+            TrieNode trieNode = this;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                TrieNode node = trieNode.childNode.get(c);
+
+                if (node == null) {
                     return false;
                 }
-
+                trieNode = node;
             }
-            idx++;
+
+            // 해당 단어로 종료하는 문자가 있는 경우 false
+            if (trieNode.terminal) {
+                if (trieNode.childNode.isEmpty()) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
-
-    private static void sort(ArrayList<String> list) {
-        Collections.sort(list);
-
-        Collections.sort(list, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
-            }
-        });
-    }
-
 }
