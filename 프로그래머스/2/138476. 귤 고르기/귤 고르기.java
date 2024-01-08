93ffcbfdd7ce9fcc7561f2dc7e75d83c95a0a167
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-01-08 12:26:24 +0900

    [level 2] Title: 귤 고르기, Time: 55.97 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        int[] arr = new int[10000001];
+        
+        for(int i = 0; i < tangerine.length; i++){
+            arr[tangerine[i]]++;
+        }
+        
+        Arrays.sort(arr);
+        int sum = 0;
+        for(int i = arr.length - 1; i >= 0; i--){
+            sum += arr[i];
+            answer++;
+            if(sum >= k){
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
