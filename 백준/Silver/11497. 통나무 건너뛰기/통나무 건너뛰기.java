810a0fe0e02d5478271c065f4d19ab4892fa3bf7
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-01-23 11:05:51 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 448 ms, Memory: 48748 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 2; i < N; i++) {
+                max = Math.max(max, arr[i] - arr[i - 2]);
+            }
+
+            sb.append(max + "\n");
+        }
+        System.out.println(sb);
+    }
+}
