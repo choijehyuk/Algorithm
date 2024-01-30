4c642b1c8d15e9bdb3ba5b2e9c05635ef4eb3161
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-01-30 10:56:52 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.47 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for(int i = 1; i <= number; i++){
+            int cnt = 0;
+            boolean flag = false;
+            for(int j = 1; j * j <= i; j++){
+                if(j * j == i) cnt++;
+                else if(i % j == 0) cnt += 2;
+            }
+            //System.out.print(cnt + " ");
+            if(cnt > limit) answer += power;
+            else answer += cnt;
+        }
+        return answer;
+    }
+}
