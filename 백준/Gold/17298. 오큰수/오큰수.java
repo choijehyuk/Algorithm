87aaa4b4f4f707d73fd7eee8c7be0b1afaf6789f
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-02-19 11:11:30 +0900

    [Gold IV] Title: 오큰수, Time: 1048 ms, Memory: 199260 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        Stack<Integer> stack = new Stack<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
