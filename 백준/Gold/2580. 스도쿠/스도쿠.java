14ac8253e9bea331af5f5ae807684e3c2e7bd69f
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-02-20 19:04:42 +0900

    [Gold IV] Title: 스도쿠, Time: 344 ms, Memory: 16512 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,99 +1,75 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 import java.util.StringTokenizer;
+
 public class Main {
-	static int [][] map;
-	static class Point{
-		int r,c;
-		Point(int r, int c){
-			this.r=r;
-			this.c=c;
-		}
-	}
-	static ArrayList<Point>list = new ArrayList<>();
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		map = new int[9][9];
-		
-		for (int r = 0; r < 9; r++) {
-			for (int c = 0; c < 9; c++) {
-				map[r][c]=sc.nextInt();
-				if(map[r][c]==0)
-					list.add(new Point(r, c));
-			}
-		}
-		//System.out.println(list.size());
-		//print(map);
-		solve(0);
-	}
-	// k : 빈좌표 index 
-	private static void solve(int k) {
-		// basis part
-		if(k==list.size()) {
-			print(map);
-			System.exit(0);
-		}
-		
-		// inductive part
-		int r = list.get(k).r;
-		int c = list.get(k).c;
-		
-		for (int i = 1; i <= 9; i++) {
-			// 가지치기
-			if(check(r,c,i)) {
-				map[r][c]=i;
-				solve(k+1);
-				map[r][c]=0;
-			}
-		
-		}
-	}
-	// 중복되는 값이 없으면 true;
-	// 아니면 false;
-	private static boolean check(int r, int c, int index) {
-		
-		for (int i = 0; i < 9; i++) {
-			if(map[r][i] == index || map[i][c] == index) return false;
-		}
-		
-		int nr = (r/3) * 3;
-		int nc = (c/3) * 3;
-		
-		for (int i = nr; i < nr+3; i++) {
-			for (int j = nc; j < nc+3; j++) {
-				if(map[i][j] == index) return false;
-			}
-		}	
-		
-		
-		return true;
-	}
-	
-	private static void print(int[][] map) {
-		for (int r = 0; r < 9; r++) {
-			for (int c = 0; c < 9; c++) {
-				System.out.print(map[r][c]+" ");
-			}
-			System.out.println();
-		}
-	}
-	
+    static int[][] board = new int[9][9];
+    static StringBuilder sb = new StringBuilder();
+    static List<Pair> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] == 0) list.add(new Pair(i, j));
+            }
+        }
+
+        solve(0);
+    }
+
+    private static void solve(int idx) {
+        if (idx == list.size()) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    sb.append(board[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+
+            System.out.println(sb.toString());
+            System.exit(0);
+        }
+
+        int x = list.get(idx).x;
+        int y = list.get(idx).y;
+
+        for (int i = 1; i <= 9; i++) {
+            if (canFill(x, y, i)) {
+                board[x][y] = i;
+                solve(idx + 1);
+                board[x][y] = 0;
+            }
+        }
+    }
+
+    private static boolean canFill(int x, int y, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[x][i] == num || board[i][y] == num) return false;
+        }
+
+        for (int i = (x / 3) * 3; i < (x / 3) * 3 + 3; i++) {
+            for (int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++) {
+                if (board[i][j] == num) return false;
+            }
+        }
+
+        return true;
+    }
 }
 
+class Pair {
+    int x;
+    int y;
 
-
-
-
-
-
-
-
-
-
-
-
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
