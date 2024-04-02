f0e527931d5ce06d2c9a22ecc73bbcca7730b73e
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-04-02 10:00:38 +0900

    [Silver V] Title: K번째 수, Time: 3312 ms, Memory: 663864 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.text.CollationElementIterator;
+import java.util.*;
+import java.util.function.DoubleToIntFunction;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(K - 1));
+    }
+}
