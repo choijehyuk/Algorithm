e1906cb86ce63200200dafa0f8a8758f2f2bb971
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-04-17 10:44:11 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 224 ms, Memory: 18180 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> o1[0] - o2[0]);
+
+        int end = 0;
+        for (int i = 0; i < N; i++) {
+            if (end < arr[i][0]) end = arr[i][0] + arr[i][1];
+            else end += arr[i][1];
+        }
+
+        System.out.println(end);
+    }
+}
