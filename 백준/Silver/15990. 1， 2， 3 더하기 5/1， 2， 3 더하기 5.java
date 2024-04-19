34e64686b0f24977aeff4e1d774dbd4ab09e648b
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-04-19 11:14:30 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 108 ms, Memory: 17456 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[100001][4];
+
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000009;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1000000009;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1000000009;
+        }
+
+        for (int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            sb.append((dp[N][1] + dp[N][2] + dp[N][3]) % 1000000009).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
