e2665a91568f383355094b198a46345b0d38163b
choijehyuk <90392777+choijehyuk@users.noreply.github.com>
2024-08-11 19:27:07 +0900

    [Unrated] Title: 생일 출력하기, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Unrated/2555. 생일 출력하기/생일 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("3/19");
+
+    }
+}
